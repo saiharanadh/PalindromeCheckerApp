2e2d358f87b403ef780501a266f17b3fb3fa85d8
saiharanadh <saiharanadhreddykaipu@gmail.com>
2026-02-18 16:17:35 +0530

    develop

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,14 @@
- class PalindromeCheckerApp {
-    public static void main (String[] args){
-        System.out.println("WELCOME TO PALINDROME CHECKER management system");
-        System.out.println("Version: 1.0");
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("======================================");
+        System.out.println("     WELCOME TO PALINDROME CHECKER    ");
+        System.out.println("======================================");
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Version          : 1.0");
+        System.out.println("--------------------------------------");
         System.out.println("System initialized successfully.");
+        System.out.println("Ready for next use case...");
+        System.out.println("======================================");
+
     }
 }
-
