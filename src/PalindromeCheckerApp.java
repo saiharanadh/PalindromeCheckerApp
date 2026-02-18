57c581885b5b7918f6b156964ca9fc64d9744606
saiharanadh <saiharanadhreddykaipu@gmail.com>
2026-02-18 15:40:20 +0530

    UC1:Welcome message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,13 @@
+class UseCase1{
+    void display(){
+        System.out.println("WELCOME TO PALINDROME CHECKER management system");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
+class UseCase2{
+
+}
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("WELCOME TO PALINDROME CHECKER management system");
