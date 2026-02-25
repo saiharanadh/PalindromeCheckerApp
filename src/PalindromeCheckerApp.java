4440353e6446073932819604b1f1648464a6af54
Jaya-Siddhartha <jayasiddhartha5@gmail.com>
2026-02-25 15:47:44 +0530

    UC3: Palindrome Check Using String Reverse

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,33 +1,43 @@
 import java.util.Scanner;
 class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
+    public static void main (String[]args) {
+        //UC1
         System.out.println("WELCOME TO PALINDROME CHECKER management system");
         System.out.println("Version: 1.0");
         System.out.println("System initialized successfully.");
 
-
+        //UC2
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string to check: ");
         String word = scanner.nextLine();
-        for (int i = 0; i < word.length() / 2; i++) {
 
-            if (word.charAt(i) != word.charAt(word.length() - 1 - j)) {
-                System.out.println("IS palindrome");
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
                 break;
             }
         }
+        if (isPalindrome) {
+            System.out.println("Is Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
 
+        //UC3
         String reversed = "";
         for (int i = word.length() - 1; i >= 0; i--) {
             reversed = reversed + word.charAt(i);
         }
-        if (word.equalsIgnoreCase(reversed)) {
-            System.out.println("The word \"" + word + "\" is a Palindrome.");
-        } else {
-            System.out.println("The word \"" + word + "\" is NOT a Palindrome.");
-        }
 
+        // Compare original and reversed strings
+        if (word.equals(reversed)) {
+            System.out.println("Is a Palindrome.");
+        } else {
+            System.out.println("NOT a Palindrome.");
+        }
         scanner.close();
 
     }
